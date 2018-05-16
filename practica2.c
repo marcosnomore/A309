f2c00b255ec3a094642975e2b1bb98ab19bd8898
marcosnomore <36762318+marcosnomore@users.noreply.github.com>
2018-05-17 00:30:54 +0200

    Update practica2.c

--- practica2.c
+++ practica2.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
@@ -12,5 +13,24 @@ void main(void)		//programa para cambiar la cadena por "hola mundo cruel"
 	strcpy(p, cadena2);
 	puts(cadena1);
 	
+			//programa para obtener los numeros de una cadena de caracteres
+	
+	char cadena[]="hola 12345 3456 44 adios"; int i=0, n, npos=0; long num; char *p=cadena;
+	//Tenemos dos opciones, cada una englobada entre comentarios.
+/*	for(;*p!=0;p++)
+	{
+		if(isdigit(*p))
+		{
+			num=strtol(p, &p, 10);
+			printf("%ld ", num);			
+		}
+	}
+*/
+
+/*	while(1==sscanf(p, "%*[^0123456789]%d%n", &n, &npos))
+	{
+		p+=npos;
+		printf("%d ", n);
+	}	*/
 	system ("PAUSE");
 }
